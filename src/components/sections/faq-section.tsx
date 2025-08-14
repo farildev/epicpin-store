dfcca6345f2af3448b1c5e89fd8c8f27e8b5bd14
Faril Mammadov <memmedovfaril@gmail.com>
2025-08-14 14:07:00 +0400

    feat : padding bottom added to faq

--- src/components/sections/faq-section.tsx
+++ src/components/sections/faq-section.tsx
@@ -2,7 +2,7 @@ import CustomAccordion from '@/components/common/custom-accordion';
 
 export default function FaqSection() {
   return (
-    <div className="flex flex-col justify-center gap-5">
+    <div className="flex flex-col justify-center gap-5 pb-5">
       <h1 className="text-2xl font-semibold">Ən çox verilən suallar</h1>
       <CustomAccordion />
     </div>
