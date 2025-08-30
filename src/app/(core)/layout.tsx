9d1df6b8985680cb0f449b0e743ceda6b6566a0b
Faril Mammadov <memmedovfaril@gmail.com>
2025-08-30 14:17:47 +0400

    feat: Vercel analytics added

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/common/header';
 import Footer from '@/components/common/footer';
-
+import { Analytics } from '@vercel/analytics/next';
 export default function MainLayout({
   children,
 }: {
@@ -9,6 +9,7 @@ export default function MainLayout({
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Header />
+      <Analytics />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
